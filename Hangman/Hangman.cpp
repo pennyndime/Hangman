5406f5d491be6b247221998f9390b4fe4160e47d
pennyndime <teranpaul22@gmail.com>
2020-11-22 19:54:46 -0600

    Added mystery word and guess list for user

--- Hangman/Hangman.cpp
+++ Hangman/Hangman.cpp
@@ -5,14 +5,16 @@
 #include <string>
 #include <fstream>
 #include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
     //Start screen
-    char start{};
+    char start;
 
+    system("CLS");
     cout << "Welcome to Hangman\n" << "by PennynDime\n";
     cout << "_______________" << endl;
     cout << "|      |       " << endl;
@@ -25,9 +27,21 @@ int main()
     cout << "Press any key to start" << endl;
     cin >> start;
 
+    //Clear screen
+    system("CLS");
+
     //Game
     //Set variables used throughout the game
-    int tries{ 6 }; //7 tries to guess correctly
+    int tries{ 6 }; //number of attempts to guess correctly
+    char guess;
+    bool correctGuess = false;
+    char previousGuesses[7]; //an array of guesses, needs to be one longer than the user can guess
+    previousGuesses[0] = '\0';
+    for (int i = 1; i < 6; i++) {
+        previousGuesses[i] = '-';
+    }
+    previousGuesses[6] = '\0';
+
     string word; //This will be the word the player has to guess
     string wordList[99];  //Array with 100 strings
 
@@ -38,25 +52,75 @@ int main()
 
     for (int i = 0; i < 99; i++) {
         randomWord >> wordList[i]; //Filling the array with the words in the list
-        cout << wordList[i] << endl;
+        //cout << wordList[i] << endl; //Testing if words are being assigned to array
     }
 
-    asdfasdfasdf
+    int randomNum = rand() % 100; // returns random number 0-99
+    word = wordList[randomNum];
+    //cout << word << endl; //Testing to see if random word in wordList is displayed
+    randomWord.close();
+
+    //Make word a mystery
+    string mysteryWord(word.length(), '*'); //Replaced each letter of the word with an '*' and stores it in mysteryWord
+    //cout << mysteryWord << endl; //Testing to see if word was replaced with '*'
+
     while (tries >= 0) {
+        //reset bool 
+        correctGuess = false;
+
+        //Display information related to the word for the player
+        cout << "The word you have to guess is: " << endl;
+        cout << mysteryWord << endl;
+        cout << "There are " << mysteryWord.length() << " letters in the word." << endl;
+        cout << "You have " << tries << " guesses to get the word correct.\n" << endl;
+        if (previousGuesses[0] == '\0') {
+            cout << endl;
+        }
+        else {
+            cout << "You have guessed: " << previousGuesses << endl;
+        }
+        
+        //get player guess
+        cout << "Guess a letter: ";
+        cin >> guess;
+        cout << "\n\n";
+
+        //Check is the guess correct
+        for (int i = 0; i < mysteryWord.length(); i++) { //for the length of the word, using mystery, do the following
+            if (word[i] == guess) { //check if the word has the guessed letter
+                mysteryWord[i] = guess;
+                correctGuess = true;
+            }
+        }
+        if (word == mysteryWord) {
+            cout << "Congrats! You got the word correct! The word was: " << mysteryWord << endl;
+            break;
+        }
+        if (correctGuess == false) {
+            tries--;
+            cout << "Sorry, " << guess << " is not part of the word." << endl;
+        }
+        else {
+            cout << "Nice! " << guess << " is one of the letters" << endl;
+        }
+    
         switch (tries) {
         case 6: {
             cout << "_______________" << endl;
             cout << "|      |       " << endl;
-            cout << "|      O       " << endl;
-            tries--;
+            cout << "|              " << endl;
+            cout << "\n\n";
             break;
         }
         case 5: {
             cout << "_______________" << endl;
             cout << "|      |       " << endl;
             cout << "|      O       " << endl;
-            cout << "|      |       " << endl;
-            tries--;
+            cout << "|              " << endl;
+            cout << "\n\n";
+            if (correctGuess == false) {
+                previousGuesses[0] = guess;
+            }
             break;
         }
         case 4: {
@@ -64,8 +128,11 @@ int main()
             cout << "|      |       " << endl;
             cout << "|      O       " << endl;
             cout << "|      |       " << endl;
-            cout << "|      |   " << endl;
-            tries--;
+            cout << "|      |       " << endl;
+            cout << "\n\n";
+            if (correctGuess == false) {
+                previousGuesses[1] = guess;
+            }
             break;
         }
 
@@ -74,9 +141,12 @@ int main()
             cout << "|      |       " << endl;
             cout << "|      O       " << endl;
             cout << "|      |       " << endl;
-            cout << "|      |   " << endl;
-            cout << "|      |   " << endl;
-            tries--;
+            cout << "|      |       " << endl;
+            cout << "|      |       " << endl;
+            cout << "\n\n";
+            if (correctGuess == false) {
+                previousGuesses[2] = guess;
+            }
             break;
         }
 
@@ -87,7 +157,10 @@ int main()
             cout << "|      |       " << endl;
             cout << "|    / | \\    " << endl;
             cout << "|   /  |  \\   " << endl;
-            tries--;
+            cout << "\n\n";
+            if (correctGuess == false) {
+                previousGuesses[3] = guess;
+            }
             break;
         }
         case 1: {
@@ -99,7 +172,10 @@ int main()
             cout << "|   /  |  \\   " << endl;
             cout << "|       \\     " << endl;
             cout << "|        \\    " << endl;
-            tries--;
+            cout << "\n\n";
+            if (correctGuess == false) {
+                previousGuesses[4] = guess;
+            }
             break;
         }
         case 0: {
@@ -111,6 +187,7 @@ int main()
             cout << "|   /  |  \\   " << endl;
             cout << "|     / \\     " << endl;
             cout << "|    /   \\    " << endl;
+            cout << "\n\n";
             cout << "Game Over!";
             tries = -1;
             break;
@@ -119,5 +196,5 @@ int main()
             cout << "Something went terribly wrong..";
         }
     }
-
+    return 0;
 }
